af372ac9276dc627ae8d7ebd081df4e647643bbf
Nurbolot <nurchik0225@gmail.com>
2024-02-25 17:04:02 +0600

    Added className for the css class to work

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import React, {Suspense} from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
 import {AboutPageAsync} from './pages/AboutPage/AboutPage.async';
 import {MainPageAsync} from './pages/MainPage/MainPage.async';
-import './styles/index.scss'
 import {useTheme} from './theme/useTheme';
+import {classNames} from './helpers/classNames/classNames';
+import './styles/index.scss'
 
 
 const App = () => {
     const {theme, toggleTheme} = useTheme()
 
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {}, [theme])}>
             <button onClick={toggleTheme}>TOGGLE</button>
             <Link to='/'>main</Link>
             <Link to='/about'>about</Link>
