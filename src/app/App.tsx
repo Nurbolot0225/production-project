e8681be5084d5d06480ed48653e67e1f19dd28bc
Nurbolot <nurchik0225@gmail.com>
2024-02-26 15:56:14 +0600

    Added Navbar component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,19 @@
-import React, {Suspense} from 'react';
+import React from 'react';
 import {Link} from 'react-router-dom';
 import {useTheme} from 'app/providers/ThemeProvider';
 import {classNames} from 'shared/lib/classNames/classNames';
 import './styles/index.scss'
 import {AppRouter} from 'app/providers/router';
-
+import {Navbar} from 'widgets/Navbar';
 
 const App = () => {
     const {theme, toggleTheme} = useTheme()
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to='/'>main</Link>
-            <Link to='/about'>about</Link>
+            <Navbar />
             <AppRouter />
+            <button onClick={toggleTheme}>TOGGLE</button>
         </div>
     );
 };
