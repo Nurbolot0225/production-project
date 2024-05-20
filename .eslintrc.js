e9cbd54008cc00989c1eea49a87390a3da06a72a
Nurbolot <nurchik0225@gmail.com>
2024-05-20 11:43:48 +0600

    add not-found page and loader

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,39 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "jest": true,
+    env: {
+        browser: true,
+        es2021: true,
+        jest: true
     },
-    "extends": [
-        "standard-with-typescript",
-        "plugin:react/recommended",
-        "plugin:i18next/recommended",
+    extends: [
+        'standard-with-typescript',
+        'plugin:react/recommended',
+        'plugin:i18next/recommended'
     ],
-    "overrides": [
+    overrides: [
         {
-            "env": {
-                "node": true
+            env: {
+                node: true
             },
-            "files": [
-                ".eslintrc.{js,cjs,tsx}"
+            extends: ['plugin:@typescript-eslint/disable-type-checked'],
+            files: [
+                '.eslintrc.{js,cjs,tsx}'
             ],
-            "parserOptions": {
-                "sourceType": "script"
+            parserOptions: {
+                sourceType: 'script'
             }
         }
     ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
     },
-    "plugins": [
-        "react",
-        "import",
-        "i18next",
+    plugins: [
+        'react',
+        'import',
+        'i18next'
     ],
-    "rules": {
-        'sort-imports': ['error', {ignoreCase: true, ignoreDeclarationSort: true}],
+    rules: {
+        'sort-imports': ['error', { ignoreCase: true, ignoreDeclarationSort: true }],
         'import/order': [
             'error',
             {
@@ -43,54 +44,54 @@ module.exports = {
                     {
                         pattern: 'react',
                         group: 'external',
-                        position: 'before',
+                        position: 'before'
                     },
                     {
                         pattern: '@src/**',
-                        group: 'internal',
-                    },
+                        group: 'internal'
+                    }
                 ],
                 pathGroupsExcludedImportTypes: ['internal', 'react'],
                 'newlines-between': 'always',
                 alphabetize: {
                     order: 'asc',
-                    caseInsensitive: true,
-                },
-            },
+                    caseInsensitive: true
+                }
+            }
         ],
-        "react/jsx-indent": [2, 4],
-        "react/jsx-indent-props": [2, 4],
-        "indent": [2, 4],
-        "max-len": [
+        'react/jsx-indent': [2, 4],
+        'react/jsx-indent-props': [2, 4],
+        indent: [2, 4],
+        'max-len': [
             'error',
             {
-                "code": 100,
+                code: 100,
                 ignoreComments: true
             }
         ],
-        "react/jsx-filename-extension": [2, { extensions: [".js", ".jsx", ".tsx"] }],
-        "import/no-unresolved": "off",
-        "import/prefer-default-export": "off",
-        "no-unused-vars": "warn",
-        "no-console": "warn",
-        "react/require-default-props": "off",
-        "react/react-in-jsx-scope": "off",
-        "react/jsx-props-no-spreading": "off",
-        "react/function-component-definition": "off",
-        "no-shadow": "off",
-        "import/extensions": "off",
-        "import/no-extraneous-dependencies": "off",
-        "no-underscore-dangle": "off",
-        "@typescript-eslint/indent": "off",
-        "@typescript-eslint/prefer-nullish-coalescing": "off",
-        "@typescript-eslint/strict-boolean-expressions": "off",
-        "@typescript-eslint/explicit-function-return-type": "off",
-        "@typescript-eslint/no-floating-promises": "off",
-        "@typescript-eslint/naming-convention": "off",
-        "react/no-deprecated": "off",
-        "i18next/no-literal-string": ['error', { markupOnly: true }],
+        'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
+        'import/no-unresolved': 'off',
+        'import/prefer-default-export': 'off',
+        'no-unused-vars': 'warn',
+        'no-console': 'warn',
+        'react/require-default-props': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-props-no-spreading': 'off',
+        'react/function-component-definition': 'off',
+        'no-shadow': 'off',
+        'import/extensions': 'off',
+        'import/no-extraneous-dependencies': 'off',
+        'no-underscore-dangle': 'off',
+        '@typescript-eslint/indent': 'off',
+        '@typescript-eslint/prefer-nullish-coalescing': 'off',
+        '@typescript-eslint/strict-boolean-expressions': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/no-floating-promises': 'off',
+        '@typescript-eslint/naming-convention': 'off',
+        'react/no-deprecated': 'off',
+        'i18next/no-literal-string': ['error', { markupOnly: true }],
     },
-    "globals": {
-        "__IS_DEV__": true,
+    globals: {
+        __IS_DEV__: true
     }
 }
--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,30 +1,26 @@
 import React, { Suspense } from 'react'
 
-import { useTranslation } from 'react-i18next'
 import { Route, Routes } from 'react-router-dom'
 import { routeConfig } from 'shared/config/routerConifg/routeConfig'
+import { PageLoader } from 'shared/ui/PageLoader/PageLoader'
 
 const AppRouter = () => {
-    const { t } = useTranslation()
-
     return (
-        <Suspense fallback={<div>{t('Loading')}</div>}>
-            <Routes>
-                {Object.values(routeConfig).map(({ element, path }) => (
-                    <Route
-                        key={path}
-                        path={path}
-                        element={(
-                            <Suspense fallback={<div>{t('Loading')}</div>}>
-                                <div className="page-wrapper">
-                                    {element}
-                                </div>
-                            </Suspense>
-                        )}
-                    />
-                ))}
-            </Routes>
-        </Suspense>
+        <Routes>
+            {Object.values(routeConfig).map(({ element, path }) => (
+                <Route
+                    key={path}
+                    path={path}
+                    element={(
+                        <Suspense fallback={<PageLoader />}>
+                            <div className="page-wrapper">
+                                {element}
+                            </div>
+                        </Suspense>
+                    )}
+                />
+            ))}
+        </Routes>
     )
 }
 
