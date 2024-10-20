dd5568925f40209e60ffb10990db12abe99246cb
Nurbolot <nurchik0225@gmail.com>
2024-10-20 20:00:48 +0600

    add lazy loading, add reducer manager, add dynamic module loader component

---
+++ src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
@@ -0,0 +1,48 @@
+import { type FC, useEffect } from 'react'
+
+import { type Reducer } from '@reduxjs/toolkit'
+import { type ReduxStoreWithManager, type StateSchemaKey } from 'app/providers/StoreProvider/config/StateSchema'
+import { useDispatch, useStore } from 'react-redux'
+
+export type ReducerList = {
+    [name in StateSchemaKey]?: Reducer
+}
+
+type ReducersListEntry = [StateSchemaKey, Reducer]
+
+interface DynamicModuleLoaderProps {
+    reducers: ReducerList
+    removeAfterUnmount?: boolean
+}
+
+export const DynamicModuleLoader: FC<DynamicModuleLoaderProps> = (props) => {
+    const {
+        children,
+        reducers,
+        removeAfterUnmount
+    } = props
+    const store = useStore() as ReduxStoreWithManager
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        Object.entries(reducers).forEach(([name, reducer]: ReducersListEntry) => {
+            store.reducerManager.add(name, reducer)
+            dispatch({ type: `@INIT ${name} reducer` })
+        })
+        return () => {
+            if (removeAfterUnmount) {
+                Object.entries(reducers).forEach(([name]: ReducersListEntry) => {
+                    store.reducerManager.remove(name)
+                    dispatch({ type: `@DESTROY ${name} reducer` })
+                })
+            }
+        }
+        // eslint-disable-next-line
+    }, [])
+
+    return (
+        <>
+            {children}
+        </>
+    )
+}
