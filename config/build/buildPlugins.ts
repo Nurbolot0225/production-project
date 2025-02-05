646e8d7a5e51288821fcec81ca279c5b8c4bb8b9
Nurbolot <nurchik0225@gmail.com>
2025-02-05 13:56:27 +0600

    production deploy

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,4 +1,5 @@
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin'
+import CopyPlugin from 'copy-webpack-plugin'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import webpack from 'webpack'
@@ -20,6 +21,11 @@ export function buildPlugins ({ paths, isDev, apiUrl, project }: BuildOptions): 
             __IS_DEV__: JSON.stringify(isDev),
             __API__: JSON.stringify(apiUrl),
             __PROJECT__: JSON.stringify(project)
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: paths.locales, to: paths.buildLocales }
+            ]
         })
     ]
 
